f1121067a467fbca5bdfcdbab8780742dea29b6e
jovijovi <mageyul@hotmail.com>
2022-04-19 14:29:53 +0800

    fix(lib): typo

--- lib/errors.ts
+++ lib/errors.ts
@@ -1,4 +1,4 @@
 export const ErrorInvalidAddress = 'invalid address';
-export const ErrorKeyFileNotExist = 'key file not exist';
+export const ErrorKeyFileNotExist = 'the key file does not exist';
 export const ErrorInvalidPassword = 'invalid password';
 export const ErrorInvalidPK = 'invalid pk';
